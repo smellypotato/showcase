66b2bc1804fb1160f0476d84e73f0e2f96249fbb
Henry Tsui <191@tranthinktech.com>
2022-05-13 18:02:33 +0800

    refactor dice

--- src/components/yahtzee/dice.tsx
+++ src/components/yahtzee/dice.tsx
@@ -1,62 +1,26 @@
 import "./dice.css";
 import { Point } from "./yahtzeeUtil";
 
+const Value: Map<Point, Array<number>> = new Map([
+    [1, [4]],
+    [2, [0, 8]],
+    [3, [2, 4, 6]],
+    [4, [0, 2, 6, 8]],
+    [5, [0, 2, 4, 6, 8]],
+    [6, [0, 2, 3, 5, 6, 8]]
+])
+
 export const Dice = (props: { point: Point, locked: boolean, onClick: Function | undefined }) => {
     const dot = (red: boolean = false) => {
         return <div className={ `dot ${red && "dot-red"}` } />
     }
 
     const point = (value: Point) => {
-        switch (value) {
-            case 1:
-                return (
-                    <div data-clickable={ `${props.onClick !== undefined}` } className={ `dice ${props.locked && "dice-locked"}` } onClick={ () => props.onClick ? props.onClick() : {} }>
-                        <div /><div /><div />
-                        <div />{ dot(true) }<div />
-                        <div /><div /><div />
-                    </div>
-                )
-            case 2:
-                return (
-                    <div data-clickable={ `${props.onClick !== undefined}` } className={ `dice ${props.locked && "dice-locked"}` } onClick={ () => props.onClick ? props.onClick() : {} }>
-                        { dot() }<div /><div />
-                        <div /><div /><div />
-                        <div /><div />{ dot() }
-                    </div>
-                )
-            case 3:
-                return (
-                    <div data-clickable={ `${props.onClick !== undefined}` } className={ `dice ${props.locked && "dice-locked"}` } onClick={ () => props.onClick ? props.onClick() : {} }>
-                        <div /><div />{ dot() }
-                        <div />{ dot() }<div />
-                        { dot() }<div /><div />
-                    </div>
-                )
-            case 4:
-                return (
-                    <div data-clickable={ `${props.onClick !== undefined}` } className={ `dice ${props.locked && "dice-locked"}` } onClick={ () => props.onClick ? props.onClick() : {} }>
-                        { dot(true) }<div />{ dot(true) }
-                        <div /><div /><div />
-                        { dot(true) }<div />{ dot(true) }
-                    </div>
-                )
-            case 5:
-                return (
-                    <div data-clickable={ `${props.onClick !== undefined}` } className={ `dice ${props.locked && "dice-locked"}` } onClick={ () => props.onClick ? props.onClick() : {} }>
-                        { dot() }<div />{ dot() }
-                        <div />{ dot() }<div />
-                        { dot() }<div />{ dot() }
-                    </div>
-                )
-            case 6:
-                return (
-                    <div data-clickable={ `${props.onClick !== undefined}` } className={ `dice ${props.locked && "dice-locked"}` } onClick={ () => props.onClick ? props.onClick() : {} }>
-                        { dot() }<div />{ dot() }
-                        { dot() }<div />{ dot() }
-                        { dot() }<div />{ dot() }
-                    </div>
-                )
-        }
+        return (
+            <div data-clickable={ `${props.onClick !== undefined}` } className={ `dice ${props.locked && "dice-locked"}` } onClick={ () => props.onClick ? props.onClick() : {} }>
+                { new Array(9).fill(null).map((_grid, i) => Value.get(value)!.includes(i) ? dot(value === 1 || value === 4) : <div />)}
+            </div>
+        )
     }
     return point(props.point);
 }
